27651bd09bd5a122c5ce0f091eb8ba36dcc5b27a
vaibhav1seth <vaibhavk774@gmail.com>
2019-03-08 01:26:39 +0530

    Done tokenization

--- Classification.py
+++ Classification.py
@@ -1,19 +1,26 @@
 import pandas as pd
+import string
+from nltk.tokenize import word_tokenize
 
 data = pd.read_csv("train.csv")
 
-#removes punctuation symbols
 
-def punctuation(string):
-    punctuation = '''''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    no_punct = ""
-    for char in string:
-        if char not in punctuation:
-            no_punct = no_punct + char
-    print(no_punct)
+# removes punctuation symbols
+
+def punctuation():
+    data['question_text'] = data['question_text'].apply(lambda x: x.lower())
+    data['question_text'] = data['question_text'].apply(lambda x: x.translate(string.punctuation))
+    data['question_text'] = data['question_text'].apply(lambda x: x.translate(string.digits))
+    #print(data['question_text'])
+
+#tokenizing the given texts
+def tokenization():
+    data['tokenized_text'] = data['question_text'].apply(word_tokenize)
+    print(data["tokenized_text"].head())
 
 
-for row in data["question_text"]:
-    punctuation(row)
-
-#print(data["question_text"].head())
+# for row in data["question_text"]:
+#   punctuation(row)
+punctuation()
+tokenization()
+# print(data["question_text"].head())
