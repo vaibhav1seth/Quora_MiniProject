a44a851a878e7f058996b7ead93c65e58c4f4db9
vaibhav1seth <33999085+vaibhav1seth@users.noreply.github.com>
2019-04-25 06:35:10 +0530

    created the model for prediction

--- Classification.py
+++ Classification.py
@@ -1,112 +1,151 @@
-import pandas as pd
-import string
-import word2vec
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import re
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Embedding
+from keras.initializers import Constant
 
-lemmatizer = WordNetLemmatizer()
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
+from sklearn.manifold import TSNE
 
-data = pd.read_csv("train.csv")
-settings = {
-	'window_size': 2,	# context window +- center word
-	'n': 10,		# dimensions of word embeddings, also refer to size of hidden layer
-	'epochs': 50,		# number of training epochs
-	'learning_rate': 0.01	# learning rate
-}
+stop = set(stopwords.words('english'))
+train = pd.read_csv('train.csv')
+print(train.head())
 
-# Initialise object
+fig,ax = plt.subplots(1,1)
+train.hist(column = 'target', ax = ax)
+ax.set_title('Number of entries classified as sincere vs insincere')
+ax.set_xticks([0,1])
+print('Percent of insincere entries %.3f %%'%(100*(sum(train['target'])/len(train))))
 
-# removes punctuation symbols
+train[train['target']==1].head()
 
-def punctuation():
-    data['question_text'] = data['question_text'].apply(lambda x: x.lower())
-    data['question_text'] = data['question_text'].apply(lambda x: x.translate(string.punctuation))
-    #data['question_text'] = data['question_text'].apply(lambda x: x.translate(string.digits))
-    #print(data['question_text'])
+n_posts = 1000
+q_S = ' '.join(train[train['target'] == 0]['question_text'].str.lower().values[:n_posts])
+q_I = ' '.join(train[train['target'] == 1]['question_text'].str.lower().values[:n_posts])
 
-#tokenizing the given texts
-def tokenization():
-    data['tokenized_text'] = data['question_text'].apply(word_tokenize)
-    print(data["tokenized_text"].head())
+wordcloud_S = WordCloud(max_font_size=None, stopwords=stop,scale = 2,colormap = 'Dark2').generate(q_S)
+wordcloud_I = WordCloud(max_font_size=None, stopwords=stop,scale = 2,colormap = 'Dark2').generate(q_I)
 
+fig, ax = plt.subplots(1,2, figsize=(20, 5))
+ax[0].imshow(wordcloud_S)
+ax[0].set_title('Top words sincere posts',fontsize = 20)
+ax[0].axis("off")
 
-def lemmatize_text(s):
+ax[1].imshow(wordcloud_I)
+ax[1].set_title('Top words INsincere posts',fontsize = 20)
+ax[1].axis("off")
 
-    s = [lemmatizer.lemmatize(word) for word in s]
-    return s
+plt.show()
 
+embeddings_index = {}
+f = open('glove.840B.300d/glove.840B.300d.txt',encoding="utf8")
+print("abjs")
+for line in f:
+    # values = line.split()
+    # word = values[0]
+    # coefs = np.asarray(values[1:], dtype='float32')
+    # embeddings_index[word] = coefs
+    values = line.split()
+    word = ''.join(values[:-300])
+    coefs = np.asarray(values[-300:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
 
+print('GloVe data loaded')
 
+#Iterate over the data to preprocess by removing stopwords
+lines_without_stopwords=[]
+for line in train['question_text'].values:
+    line = line.lower()
+    line_by_words = re.findall(r'(?:\w+)', line, flags = re.UNICODE) # remove punctuation ans split
+    new_line=[]
+    for word in line_by_words:
+        if word not in stop:
+            new_line.append(word)
+    lines_without_stopwords.append(new_line)
+texts = lines_without_stopwords
 
+print(texts[0:5])
 
-# for row in data["question_text"]:
-#   punctuation(row)
-punctuation()
-tokenization()
-#lemmatize_text()
-# print(data["question_text"].head())
-data = data.assign(col_lemma=data['tokenized_text'].apply(lambda x: lemmatize_text(x)))
-#print(data)
+MAX_NUM_WORDS = 1000
+MAX_SEQUENCE_LENGTH = 100
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
 
-w2v = word2vec()
-# Numpy ndarray with one-hot representation for [target_word, context_words]
-training_data = w2v.generate_training_data(settings,data['col_lemma'])
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
 
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
 
-class word2vec():
-  def __init__(self):
-    self.n = settings['n']
-    self.lr = settings['learning_rate']
-    self.epochs = settings['epochs']
-    self.window = settings['window_size']
+labels = to_categorical(np.asarray(train['target']))
+print(data.shape)
+print(labels.shape)
 
-  def generate_training_data(self, settings, corpus):
-    # Find unique word counts using dictonary
-    word_counts = defaultdict(int)
-    for row in corpus:
-      for word in row:
-        word_counts[word] += 1
-    ## How many unique words in vocab? 9
-    self.v_count = len(word_counts.keys())
-    # Generate Lookup Dictionaries (vocab)
-    self.words_list = list(word_counts.keys())
-    # Generate word:index
-    self.word_index = dict((word, i) for i, word in enumerate(self.words_list))
-    # Generate index:word
-    self.index_word = dict((i, word) for i, word in enumerate(self.words_list))
+## EMBEDDING_DIM =  ## seems to need to match the embeddings_index dimension
+EMBEDDING_DIM = embeddings_index.get('a').shape[0]
+num_words = min(MAX_NUM_WORDS, len(word_index)) + 1
+embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+for word, i in word_index.items():
+    if i > MAX_NUM_WORDS:
+        continue
+    embedding_vector = embeddings_index.get(word) ## This references the loaded embeddings dictionary
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
 
-    training_data = []
-    # Cycle through each sentence in corpus
-    for sentence in corpus:
-      sent_len = len(sentence)
-      # Cycle through each word in sentence
-      for i, word in enumerate(sentence):
-        # Convert target word to one-hot
-        w_target = self.word2onehot(sentence[i])
-        # Cycle through context window
-        w_context = []
-        # Note: window_size 2 will have range of 5 values
-        for j in range(i - self.window, i + self.window+1):
-          # Criteria for context word 
-          # 1. Target word cannot be context word (j != i)
-          # 2. Index must be greater or equal than 0 (j >= 0) - if not list index out of range
-          # 3. Index must be less or equal than length of sentence (j <= sent_len-1) - if not list index out of range 
-          if j != i and j <= sent_len-1 and j >= 0:
-            # Append the one-hot representation of word to w_context
-            w_context.append(self.word2onehot(sentence[j]))
-            # print(sentence[i], sentence[j]) 
-            # training_data contains a one-hot representation of the target word and context words
-        training_data.append([w_target, w_context])
-    return np.array(training_data)
+# load pre-trained word embeddings into an Embedding layer
+# note that we set trainable = False so as to keep the embeddings fixed
+embedding_layer = Embedding(num_words,
+                            EMBEDDING_DIM,
+                            embeddings_initializer=Constant(embedding_matrix),
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
 
-  def word2onehot(self, word):
-    # word_vec - initialise a blank vector
-    word_vec = [0 for i in range(0, self.v_count)] # Alternative - np.zeros(self.v_count)
-    # Get ID of word from word_index
-    word_index = self.word_index[word]
-    # Change value from 0 to 1 according to ID of the word
-    word_vec[word_index] = 1
-    return word_vec
+## Peeking at the embedding matrix values
+print(embedding_matrix.shape)
+plt.plot(embedding_matrix[16])
+plt.plot(embedding_matrix[37])
+plt.plot(embedding_matrix[18])
+plt.title('example vectors')
 
+model = Sequential()
+model.add(Embedding(num_words, 300, input_length=100, weights= [embedding_matrix], trainable=False))
 
+model.add(Dropout(0.2))
+model.add(Conv1D(64, 5, activation='relu'))
+model.add(MaxPooling1D(pool_size=4))
+model.add(LSTM(100))
+model.add(Dense(2, activation='sigmoid'))
 
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+## Fit train data
+print(labels.shape)
+model.fit(data, np.array(labels), validation_split=0.1, epochs = 1)
+from sklearn.manifold import TSNE
+## Get weights
+embds = model.layers[0].get_weights()[0]
+## Plotting function
+## Visualize words in two dimensions
+tsne_embds = TSNE(n_components=2).fit_transform(embds)
+
+plt.plot(tsne_embds[:,0],tsne_embds[:,1],'.')
+plt.show()
+
+test = pd.read_csv('test.csv')
+print(test.head())
+
+#iske baad phass rha dont know wht to put inside that predict wala bracket
+pred = model.predict()
+
+pred = np.round(pred)
+
+print(test)
